d9e61398d67d890880d0f15c2821745e986ef3ec
HS-118360061 <46994790+HS-118360061@users.noreply.github.com>
2022-02-14 10:29:33 +0000

    Update typeWriter.js

--- First-Anniversary-of-Love-gh-pages/js/typeWriter.js
+++ First-Anniversary-of-Love-gh-pages/js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey, Laura";
+let text2 = "Happy Valentines Day!!!"
 let speed = 100;
 
 function typeWriter(text, para){
